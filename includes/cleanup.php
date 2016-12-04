68732d5eecf981b26eeaf395a9c52a7c4c1000bb
bymattlee <fivep0int9@gmail.com>
2016-12-03 16:52:00 -0800

    Clean up WP head

--- includes/cleanup.php
+++ includes/cleanup.php
@@ -7,19 +7,21 @@
 	// Clean up Wordpress head
 	function head_cleanup() {
 
-		remove_action( 'wp_head', 'rsd_link' ); // EditURI link.
-		remove_action( 'wp_head', 'feed_links_extra', 3 ); // Category feed links.
-		remove_action( 'wp_head', 'feed_links', 2 ); // Post and comment feed links.
-		remove_action( 'wp_head', 'wlwmanifest_link' ); // Windows Live Writer.
-		remove_action( 'wp_head', 'index_rel_link' ); // Index link.
-		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // Previous link.
-		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // Start link.
-		remove_action( 'wp_head', 'rel_canonical', 10, 0 ); // Canonical.
-		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 ); // Shortlink.
-		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for adjacent posts.
-		remove_action( 'wp_head', 'wp_generator' ); // WP version.
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); // Emoji detection script.
-		remove_action( 'wp_print_styles', 'print_emoji_styles' ); // Emoji styles.
+		remove_action( 'wp_head', 'rsd_link' ); // EditURI link
+		remove_action( 'wp_head', 'rest_output_link_wp_head', 10 ); // Remove REST API link
+		remove_action( 'wp_head', 'feed_links_extra', 3 ); // Category feed links
+		remove_action( 'wp_head', 'feed_links', 2 ); // Post and comment feed links
+		remove_action( 'wp_head', 'wlwmanifest_link' ); // Windows Live Writer
+		remove_action( 'wp_head', 'index_rel_link' ); // Index link
+		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // Previous link
+		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // Start link
+		remove_action( 'wp_head', 'rel_canonical', 10, 0 ); // Canonical
+		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 ); // Shortlink
+		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for adjacent posts
+		remove_action( 'wp_head', 'wp_generator' ); // WP version
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); // Emoji detection script
+		remove_action( 'wp_print_styles', 'print_emoji_styles' ); // Emoji styles
+		add_filter( 'emoji_svg_url', '__return_false' ); // Remove Emoji DNS prefetch
 
 	}
 	add_action( 'init', 'head_cleanup' );
