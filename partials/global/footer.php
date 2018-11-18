8c251bc044cfbf761a4712533e3f4585f2f4ca6f
bymattlee <fivep0int9@gmail.com>
2018-11-17 23:33:54 -0800

    Add conditionals to menu display

--- partials/global/footer.php
+++ partials/global/footer.php
@@ -1,14 +1,14 @@
 <footer class="c-footer" role="contentinfo">
 	<h4><?php printf( '%1$s %2$s %3$s', '&copy;', date( 'Y' ), get_bloginfo() ); ?></h4>
 	<nav class="o-site_nav" role="navigation">
-		<?php 
-			wp_nav_menu( 
-				array( 
+		<?php
+			if ( has_nav_menu( 'footer_menu' ) ) :
+				wp_nav_menu( array( 
 					'theme_location' => 'footer_menu', 
 					'menu_class' => 'o-site_nav-menu',
 					'walker' => new Site_Nav_Walker()
-				) 
-			);
+				) );
+			endif;
 		?>
 	</nav>
 </footer>
--- partials/global/header.php
+++ partials/global/header.php
@@ -1,14 +1,16 @@
 <header class="c-header" role="banner">
 	<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo(); ?></a></h1>
 	<nav class="o-site_nav" role="navigation">
-		<?php 
-			wp_nav_menu( 
-				array( 
-					'theme_location' => 'main_menu', 
-					'menu_class' => 'o-site_nav-menu',
-					'walker' => new Site_Nav_Walker()
-				) 
-			);
+		<?php
+			if ( has_nav_menu( 'main_menu' ) ) :
+				wp_nav_menu( 
+					array( 
+						'theme_location' => 'main_menu', 
+						'menu_class' => 'o-site_nav-menu',
+						'walker' => new Site_Nav_Walker()
+					)
+				);
+			endif;
 		?>
 	</nav>
 	<?php get_search_form(); ?>
