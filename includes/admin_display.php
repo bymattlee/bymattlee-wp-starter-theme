ed1a9ab2f59a8d6b16465223119772218057b2c1
bymattlee <fivep0int9@gmail.com>
2019-04-14 16:23:17 -0700

    Update posts to news label in admin

--- includes/admin_display.php
+++ includes/admin_display.php
@@ -60,4 +60,24 @@
 	}
 	add_action( 'wp_before_admin_bar_render', 'bml_remove_comments_from_admin_bar' );
 
+	// Rename Posts label to News
+	function bml_change_post_object() {
+		global $wp_post_types;
+		$labels = &$wp_post_types[ 'post' ]->labels;
+		$labels->name = 'News';
+		$labels->singular_name = 'News';
+		$labels->add_new = 'Add New';
+		$labels->add_new_item = 'Add New News';
+		$labels->edit_item = 'Edit News';
+		$labels->new_item = 'News';
+		$labels->view_item = 'View News';
+		$labels->search_items = 'Search News';
+		$labels->not_found = 'No News found';
+		$labels->not_found_in_trash = 'No News found in Trash';
+		$labels->all_items = 'All News';
+		$labels->menu_name = 'News';
+		$labels->name_admin_bar = 'News';
+	}
+	add_action( 'init', 'bml_change_post_object' );
+
 ?>
