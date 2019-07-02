f2a449fffa00260309581bd7b307774c4d40ba54
bymattlee <fivep0int9@gmail.com>
2019-07-02 11:51:54 -0700

    Update acf formatting

--- includes/acf.php
+++ includes/acf.php
@@ -9,23 +9,23 @@
 		// Add Global Options tab to WP Admin
 		acf_add_options_page( array(
 			'menu_title'	=> __( 'Global Options', 'famehouse' ),
-			'page_title' 	=> __( 'Global Options', 'famehouse' ),
-			'menu_slug' 	=> 'global-options',
-			'position'	 	=> '31',
+			'page_title'	=> __( 'Global Options', 'famehouse' ),
+			'menu_slug'		=> 'global-options',
+			'position'		=> '31',
 			'capability'	=> 'edit_posts',
 			'icon_url'		=> 'dashicons-admin-generic',
 		));
 		
 		// Add Social Options section under the Global Options tab
 		acf_add_options_sub_page( array(
-			'page_title' 	=> __( 'Social Options', 'bymattlee' ),
+			'page_title'	=> __( 'Social Options', 'bymattlee' ),
 			'menu_title'	=> __( 'Social', 'bymattlee' ),
 			'parent_slug'	=> 'global-options',
 		));
 
 		// Add Code Options section under the Global Options tab
 		acf_add_options_sub_page( array(
-			'page_title' 	=> __( 'Code Options', 'bymattlee' ),
+			'page_title'	=> __( 'Code Options', 'bymattlee' ),
 			'menu_title'	=> __( 'Code', 'bymattlee' ),
 			'parent_slug'	=> 'global-options',
 		));
