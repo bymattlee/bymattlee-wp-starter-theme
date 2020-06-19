2211fd49c043755194354e70c124b55eb38cc6e5
bymattlee <fivep0int9@gmail.com>
2020-06-18 20:54:13 -0700

    Create directory for js utilities and simplify global

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -4,8 +4,6 @@
 
 import { init as globalInit } from './modules/global.js';
 import { init as richTextEditorInit } from './modules/richTextEditor.js';
-import { init as mobileLockInit } from './modules/mobileLock.js';
 
 globalInit();
 richTextEditorInit();
-mobileLockInit();
